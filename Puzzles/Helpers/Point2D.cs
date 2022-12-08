e9c9286d22af58cfe61f2e56478eee6a833c4dfe
Danny Johansen <techide@gmail.com>
2022-12-09 00:40:22 +0100

    Yoinked from gnarf

--- Puzzles/Helpers/Point2D.cs
+++ Puzzles/Helpers/Point2D.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Puzzles.Helpers
+﻿namespace Puzzles.Helpers
 {
     internal class Point2D
     {
